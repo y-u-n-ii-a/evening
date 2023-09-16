28d7201430f11835327946e6e57cb9a1a7f667cf
Yuniia Olkhova <yuniia.olkhova@pfizer.com>
2023-09-17 02:19:59 +0300

    Specify required versions

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+  }
+
+  cloud {
+    organization = "yuniiaolkhova"
+    workspaces {
+      name = "evening"
+    }
+  }
+}
